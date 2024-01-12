150a2dc2e5448f07dce525f931404f305179ed0d
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-01-12 08:35:13 +0000

    rm mirte-control

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ set -e
 
 vcs import < src/ros2.repos src
 sudo apt-get update
+rm src/mirte-ros-packages/mirte-control/ -rf || true
 rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -y --rosdistro=$ROS_DISTRO
