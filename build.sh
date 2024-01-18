38fcfb0a20aa8d42d91c3b5057bc3575c708e7e4
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-01-18 15:10:02 +0000

    upd

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ BUILD_TYPE=RelWithDebInfo
 colcon build \
         --merge-install \
         --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
+        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On -GNinja" \
         -Wall -Wextra -Wpedantic \
         --parallel-workers 4
